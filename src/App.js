f8bd4c77938077869d1f951293ae3b7807cfcf1d
bamzdsptd <bambangdnr09@gmail.com>
2020-02-05 11:03:23 +0700

    First Impression about React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Nav, Navbar, Table} from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Navbar bg="light" variant="light" fixed="top" sticky="bottom">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="#home">Home</Nav.Link>
+      <Nav.Link href="#features">Features</Nav.Link>
+      <Nav.Link href="#pricing">Pricing</Nav.Link>
+      <Nav.Link href="#pricing">Disabled</Nav.Link>
+    </Nav>
+  </Navbar>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  <Table class="Table">
+  <thead>
+    <tr>
+      <th>#</th>
+      <th>First</th>
+      <th>Last</th>
+      <th>Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>1</td>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Larry</td>
+      <td>The Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</Table>
       </header>
     </div>
   );
